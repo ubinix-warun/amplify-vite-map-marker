ef5b21aa0809560bc94dfb9ae701c7ee96846c46
ubinix-warun <warun@ubinix.com>
2024-05-22 00:17:35 +0700

    Add 'create-vite-pwa and amplify init'.

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'amplify-vite-map-marker',
+      short_name: 'map-marker',
+      description: 'An interactive map PWA built with Vite, React, and TypeScript, featuring smooth marker placement and potential integration with AWS Amplify Geo for advanced location-based features.',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
